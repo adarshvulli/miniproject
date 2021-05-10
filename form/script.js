16d2a5999362aac97c2311b47dea2fa7271145e1
adarsh vulli <adarshvulli007@gmail.com>
2021-05-10 18:57:34 +0530

    form

--- form/script.js
+++ form/script.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
- $('.next-btn').click(function() {
-     $('#form-wrap').animate({
-     'marginTop' : "-=148px"
-     });
- });
+    $('.next-btn').click(function() {
+        $('#form-wrap').animate({
+        'marginTop' : "-=148px"
+        });
+    });
 });
 
+// $(document).ready(function() {
+//     $('.next-btn').click(function() {
+//         $('.progress-bar').animate({
+//         'marginLeft' : "+=20%"
+//         });
+//     });
+// });
